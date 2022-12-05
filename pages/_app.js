71aece3f703f049718ab6815b73cf685c1b7bc03
Luka Pupic <luka.pupic@5x9networks.com>
2022-12-05 14:34:49 +0100

    Defined main layout with main header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import Layout from '../layout/layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Film festival app</title>
+        <meta name='description' content='Film festival app' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
